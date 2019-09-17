2614e5a8801c47748abe0ea2e0c79f63e28a2efb
adrianjcorrea <adrianjcorrea007@gmail.com>
2019-09-17 17:02:36 -0500

    Selecting element by ID for JQUERY

---
+++ JQuery/01-Selector.js
@@ -0,0 +1,13 @@
+//Function to check jquery lib is being ablilitated correrctly.
+
+//$(document).ready(function(){
+  //console.log("JQuery CDN script working")
+//});
+
+$(document).ready(function(){
+  var red = $("#red").css("background", "red").css("color", "white");
+
+  var blue = $("#blue").css("background", "blue");
+  $("#yellow").css( "background", "yellow").css("color", "green");
+
+});
--- JQuery/01-Selectors.js
+++
@@ -1,5 +0,0 @@
-//Function to check jquery lib is being ablilitated correrctly.
-
-//    $(document).ready(function(){
-//     console.log("JQuery CDN script working")
-//})
