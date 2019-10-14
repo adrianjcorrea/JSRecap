2a40380d43b17a5078abb561a8ea0b809402467e
adrianjcorrea <adrianjcorrea007@gmail.com>
2019-10-14 16:19:38 -0500

    Implemented hover evect with JS and JQuery

--- JQuery/02-Events.js
+++ JQuery/02-Events.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
-    //MouseOver and MouseOut.
+    //MOUSEOVER AND MOUSEOUT.
     var box = $('#box');
-    box.mouseover(function(){
-        $(this).css("background","red")
-    });
+    // box.mouseover(function(){
+    //     $(this).css("background","red")
+    // });
 
-    box.mouseout(function(){
-        $(this).css("background","green")
-    });
+    // box.mouseout(function(){
+    //     $(this).css("background","green")
+    // });
+    
+    function changeRed() {
+        $(this).css("background","red");
+    }
+
+    function changeGreen() {
+        $(this).css("background", "green");
+    }
+    //HOVER EFFECT
+    box.hover(changeRed,changeGreen)
 });
